8c5494f464b6f2901dfa1862037ada03b6f9ecc9
Dmitrii Goncharov <homeboundx@gmail.com>
2022-06-25 23:58:39 +0200

    Introduce NFT configuration

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,19 @@
+import sdk from './1-initialize-sdk.js';
+import { readFileSync } from 'fs';
+
+const editionDrop = sdk.getEditionDrop('0x66916F4eB59c762eD10582e33dF94E3376bF7439');
+
+(async () => {
+    try {
+        await editionDrop.createBatch([
+            {
+                name: 'ThisDAO Membership',
+                description: 'This NFT grants you membership in ThisDAO',
+                image: readFileSync('scripts/assets/DAOLogo.svg'),
+            }
+        ]);
+        console.log('✅ Successfully created a new NFT in the drop');
+    } catch (error) {
+        console.error('Failed to create NFT:', error);
+    }
+})();
